d314f0597fbf6f46fc04613f7fdfa84093ec5fd6
zphyrj <46908127+zphyrj@users.noreply.github.com>
2021-05-27 22:54:45 -0700

    made logo float to the left

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 const NavBar= () => {
     return (  
     <div className="topnav">
-        <Link to = '/ ' style = {{backgroundColor: "black", color: '#D4AF37'}} >Sign Out</Link>
-        <Link to="/help">Help</Link>
         <Link className="active" to ="/home"> Home</Link>
+        <Link to="/help">Help</Link>
+        <Link to = '/signout' style = {{backgroundColor: "black", color: '#D4AF37'}} >Sign Out</Link>
         <Link to='/' id='topNavIcon'><img src="./transparent.png" className='topNavIcon' alt = "logo"/></Link> {/*  why isnt logo showing  */}
     </div>
 
