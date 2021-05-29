9c77857ac79b406a7dfc20c159e25959fc0347c9
Ashwin Talwalkar <atalwalkar719@gmail.com>
2021-05-29 08:21:22 -0700

    Fixed Order

--- src/pages/reviewfinder/FindReview.js
+++ src/pages/reviewfinder/FindReview.js
@@ -1,9 +1,9 @@
 const FindReview = () => {
     return ( 
-    <div className = "mainText" style = {{marginLeft: '24%', marginTop: '15%'}}>
-        <b style = {{borderBottom: '2px solid #D4AF37'}}>
+    <div className = "mainText" style = {{marginLeft: '24%', marginTop: '15%', }}>
+        <h1 style = {{borderBottom: '2px solid #D4AF37', width: '75%'}} >
             Welcome to the Review Forum
-        </b>
+        </h1>
     <form>
         <input type="text" list = "classes" placeholder = "Search by Class..." style ={{fontSize: '17px'}}></input>
         <input type= "submit" id = "subButton" className = "submit" value = "Search"></input>
