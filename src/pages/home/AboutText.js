69ef8bf69babe5a03e07f2168c204c10250a3248
zphyrj <46908127+zphyrj@users.noreply.github.com>
2021-06-02 18:38:39 -0700

    changed font to be sans-serif all the way

--- src/pages/home/AboutText.js
+++ src/pages/home/AboutText.js
@@ -1,9 +1,9 @@
 const AboutText = () => {
     return (  
-<div className = "aboutText" style= {{marginTop: '2%'}}>
-    <h1>
+<div className = "aboutText">
+    <p>
         MV Xperience is a resource that provides students with reviews and advice from real, current, and previous MVHS students, giving students a better idea of the commitment level needed, course loads of different classes, and composite schedules.
-    </h1> 
+    </p> 
 </div>
 );
 }
--- src/pages/home/MainText.js
+++ src/pages/home/MainText.js
@@ -1,11 +1,11 @@
 const MainText = () => {
     return (
     <div className = "mainText">
-        <h1 style = {{borderBottom: '2px solid #D4AF37'}}>
+        <h1>
             Search Reviews
         </h1>
         <form>
-            <input type="text" placeholder = "Search by Class..." style = {{fontSize: '17px'}} />
+            <input type="text" placeholder = "Search by Class..."/>
             <input type= "submit" id = "subButton" className = "submit" value = "Search"/>
         </form>  
     </div>   
