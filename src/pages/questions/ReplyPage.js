a8b58189ae62b3a11fe4515be4af96bde13c0c2d
SonavAgarwal <42980809+SonavAgarwal@users.noreply.github.com>
2021-06-06 16:23:48 -0700

    Update ReplyPage.js

--- src/pages/questions/ReplyPage.js
+++ src/pages/questions/ReplyPage.js
@@ -1,54 +1,55 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React from "react";
+import { useParams } from "react-router";
 import db from "../firebase";
 import { useEffect, useState } from "react";
 
-import AddReplies from './addReplies.js'
-
+import AddReplies from "./addReplies.js";
 
 const ShowReplies = (props) => {
-
     const { id } = useParams();
     const [replies, setReplies] = useState([]);
-
+    const [question, setQuestion] = useState({}); // initial state with empty object
 
     useEffect(() => {
-      var tempArray = [];
-      db.collection(`questions/${id}/replies`)
-        .get()
-        .then((querySnapshot) => {
-           querySnapshot.forEach((doc) => {
-             // doc.data() is never undefined for query doc snapshots
-             tempArray.push(doc);
-           });
-           setReplies(tempArray);
-         })
-         .catch((error) => {
-           console.log("Error getting documents: ", error);
-         });
-     }, []);
+        var tempArray = [];
+        db.collection(`questions/${id}/replies`)
+            .get()
+            .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    // doc.data() is never undefined for query doc snapshots
+                    tempArray.push(doc);
+                });
+                setReplies(tempArray);
+            })
+            .catch((error) => {
+                console.log("Error getting documents: ", error);
+            });
 
-     const question = db.collection('questions').data().questionsAsked;
-     
+        db.collection("questions") // running in useEffect to get the data only once
+            .doc(id)
+            .get()
+            .then((doc) => {
+                // getting data is an async operation, so you need a callback
+                console.log(doc.data());
+                setQuestion(doc.data()); // gets the data and saves in the question state
+            });
+    }, [id]); // reruns the useeffect if the id somehow changes
 
-  
+    // const question = db.collection("questions").data().questionsAsked;
 
-     return (  
-     <div>
-       <h1 style = {{marginLeft: '7%'}}>{question} </h1> {/*Help!*/}
-          <AddReplies/>
-       <ul>  
-           {replies.map((rep) => (
-             <div className = 'questionsPosted'>
-                 <li>{rep.data().replies} </li>
-             </div>
- 
-           ))}
-         </ul>
-     </div>
-     )
-}
+    return (
+        <div>
+            <h1 style={{ marginLeft: "7%" }}>{question.questionsAsked} </h1> {/*Help!*/} {/* Gets the field from the data */}
+            <AddReplies />
+            <ul>
+                {replies.map((rep) => (
+                    <div className='questionsPosted'>
+                        <li>{rep.data().replies} </li>
+                    </div>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
-
- 
 export default ShowReplies;
