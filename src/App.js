6b527307a377124b22e9d8fd86e127c1468b31da
SonavAgarwal <42980809+SonavAgarwal@users.noreply.github.com>
2021-06-26 11:56:43 -0700

    navbar styling in progress

--- src/App.js
+++ src/App.js
@@ -14,51 +14,51 @@ import firebase from "./pages/firebase.js";
 // see this is my home page and it goes to....
 
 function App() {
-  const [user, setUser] = useState(firebase.auth().currentUser);
-  firebase.auth().onAuthStateChanged((userIn) => {
-    console.log("herererer");
-    setUser(userIn);
-  });
-  return (
-    <Router>
-      <div className="App">
-        {/* <Navbar user={user} /> */}
-        <div className="content">
-          <Switch>
-            <Route exact path="/dashboard">
-              <Dashboard user={user}></Dashboard>
-            </Route>
-            <Route exact path="/">
-              {" "}
-              {/*Need firebase auth here and other homescreen*/}
-              <Default />
-            </Route>
-            <Route exact path="/home">
-              {" "}
-              {/*Route for home page*/}
-              <Home user={user} />
-            </Route>
-            <Route exact path="/questionForum">
-              <QuestionList user={user} />
-            </Route>
-            <Route exact path="/questionForum/:id">
-              <ReplyList />
-            </Route>
-            <Route exact path="/reviewfinder">
-              <ReviewFinder />
-            </Route>
-            <Route exact path="/reviewSubmitter">
-              <SubmitReview />
-            </Route>
-            <Route exact path="/About"></Route>
-            <Route exact path="/logout">
-              <Logout></Logout>
-            </Route>
-          </Switch>
-        </div>
-      </div>
-    </Router>
-  );
+    const [user, setUser] = useState(firebase.auth().currentUser);
+    firebase.auth().onAuthStateChanged((userIn) => {
+        console.log("herererer");
+        setUser(userIn);
+    });
+    return (
+        <Router>
+            <div className="App">
+                <Navbar user={user} />
+                <div className="content">
+                    <Switch>
+                        <Route exact path="/dashboard">
+                            <Dashboard user={user}></Dashboard>
+                        </Route>
+                        <Route exact path="/">
+                            {" "}
+                            {/*Need firebase auth here and other homescreen*/}
+                            <Default />
+                        </Route>
+                        <Route exact path="/home">
+                            {" "}
+                            {/*Route for home page*/}
+                            <Home user={user} />
+                        </Route>
+                        <Route exact path="/questionForum">
+                            <QuestionList user={user} />
+                        </Route>
+                        <Route exact path="/questionForum/:id">
+                            <ReplyList />
+                        </Route>
+                        <Route exact path="/reviewfinder">
+                            <ReviewFinder />
+                        </Route>
+                        <Route exact path="/reviewSubmitter">
+                            <SubmitReview />
+                        </Route>
+                        <Route exact path="/About"></Route>
+                        <Route exact path="/logout">
+                            <Logout></Logout>
+                        </Route>
+                    </Switch>
+                </div>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,25 +1,31 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import firebase from "./firebase.js";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import "../styles/navbar.css";
 const NavBar = (params) => {
-	const [user, setUser] = useState(firebase.auth().currentUser)
-	document.addEventListener("scroll", () => {
-  		document.documentElement.dataset.scroll = window.scrollY;
-		return true;
-	});
-	console.log('rerendering navbar')
-	document.documentElement.dataset.scroll = window.scrollY;
-	
-    return (  
-    <nav className="topnav">
-        <Link className="active" to ="/home"> Home</Link>
-        <Link to="/help">Help</Link>
-		<Link to = '/about'>About</Link>
-		{params.user && (<Link to = '/logout'>Sign Out</Link>)}
-        <Link to='/home' id='topNavIcon'><img src="/transparent.png" className='topNavIcon' alt = "logo"/></Link> 
-    </nav>
+    const [user, setUser] = useState(firebase.auth().currentUser);
+    // document.addEventListener("scroll", () => {
+    //     document.documentElement.dataset.scroll = window.scrollY;
+    //     return true;
+    // });
+    // console.log("rerendering navbar");
+    // document.documentElement.dataset.scroll = window.scrollY;
 
-        
+    return (
+        <nav className='navbar'>
+            <div className='navbarContent'>
+                {/* <Link className='active' to='/home'>
+                    {" "}
+                    Home
+                </Link>
+                <Link to='/help'>Help</Link>
+                <Link to='/about'>About</Link>
+                {params.user && <Link to='/logout'>Sign Out</Link>}
+                <Link to='/home' id='topNavIcon'>
+                    <img src='/transparent.png' className='topNavIcon' alt='logo' />
+                </Link> */}
+            </div>
+        </nav>
     );
-}
+};
 export default NavBar;
